091dbdf62bf4d1d7e3a64291a0673faab4f25553
Liry <learainccc@outlook.com>
2025-06-19 14:17:05 +0800

    更新配置

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
+import { defineCustom } from '@liry-ccc/common-config';
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
-const defineConfig = (config: NextConfig) => {
-  return config;
-};
+const defineConfig = defineCustom<NextConfig>;
 
 const config = defineConfig({
   pageExtensions: ['tsx', 'ts', 'md', 'mdx'],
