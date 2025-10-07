ec164181787eba7aaa1bfd987218f74a8df67096
Liry <learainccc@outlook.com>
2025-10-07 19:26:07 +0800

    更改项目入口，

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
+import 'normalize.css';
 import App from './App.vue';
-import 'normalize.css'
 import router from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+const container = document.createElement('div');
+const main = () => {
+  app.mount(container);
+  document.body.appendChild(container);
+};
 
-app.mount('#app');
+main();
