2676a45198125279c1cfb90e1d239727d0bca541
Liry <learainccc@outlook.com>
2025-12-29 01:08:49 +0800

    fix: fix type error

--- src/pages/gomoku/components/board/index.test.tsx
+++ src/pages/gomoku/components/board/index.test.tsx
@@ -22,13 +22,13 @@ describe('GameBoard', () => {
     const handleClick = vi.fn();
     render(<GameBoard board={board} onCellClick={handleClick} />);
     const firstCell = screen.getAllByRole('button')[0];
-    firstCell.click();
+    firstCell?.click();
     expect(handleClick).toHaveBeenCalledWith({ row: 0, col: 0 });
   });
 
   it('should not call onCellClick when cell is occupied', () => {
     const board = createEmptyBoard();
-    board[0][0] = PLAYER.BLACK;
+    board[0]![0] = PLAYER.BLACK;
     const handleClick = vi.fn();
     render(<GameBoard board={board} onCellClick={handleClick} />);
     const stone = screen.getByLabelText('Black stone');
@@ -41,14 +41,14 @@ describe('GameBoard', () => {
     const handleClick = vi.fn();
     render(<GameBoard board={board} onCellClick={handleClick} disabled />);
     const firstCell = screen.getAllByRole('button')[0];
-    firstCell.click();
+    firstCell?.click();
     expect(handleClick).not.toHaveBeenCalled();
   });
 
   it('should render stones correctly', () => {
     const board = createEmptyBoard();
-    board[0][0] = PLAYER.BLACK;
-    board[0][1] = PLAYER.WHITE;
+    board[0]![0] = PLAYER.BLACK;
+    board[0]![1] = PLAYER.WHITE;
     render(<GameBoard board={board} onCellClick={vi.fn()} />);
     expect(screen.getByLabelText('Black stone')).toBeTruthy();
     expect(screen.getByLabelText('White stone')).toBeTruthy();
